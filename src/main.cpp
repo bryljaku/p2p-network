c43a66eb071f900151166c4d4f06dc576cb0ff7f
p.zmyslony <p.zmysl@gmail.com>
2020-03-21 17:30:58 +0100

    working static lib and log test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "iostream"
 #include "pthread.h"
-
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/syslog_sink.h"
+void syslog_example()
+{
+    // w konsoli "tail -f /var/log/syslog" i powinno byc widac ze wchodzi
+    std::string ident = "spdlog-example";
+    auto syslog_logger = spdlog::syslog_logger_mt("syslog", ident, LOG_PID);
+    syslog_logger->warn("This is warning that will end up in syslog.");
+}
 int main() {
-    std::cout << "Test";
+    spdlog::warn("test");
+    syslog_example();
     return 0;
 }
