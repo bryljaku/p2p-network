4edc90913d7e0b0570519eb168496fbb4e6c8d1d
jbryl7 <jakub.bryl7@gmail.com>
2020-04-10 18:35:32 +0200

    corrected typos

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 #include "iostream"
 #include "pthread.h"
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/syslog_sink.h"
-#include "database/Database.h"
-
-
-int main() {
-    spdlog::warn("client");
-    auto db = Database();
-    Id testId = 0;
-    db.addFile(File(testId, 50000, "sys/path"));
-    auto f = db.getFile(testId);
-    db.removeFile(testId);
-    return 0;
-=======
-#include "sharedUtils.h"
+#include <sharedUtils.h>
 #include "networking/CSocket.h"
 #include "networking/RequestHandler.h"
 #include "networking/SSocket.h"
@@ -24,12 +9,10 @@ int main() {
 #define TRACKER_MAX_MESSAGE_SIZE 128*1024	// in bytes
 
 int main(int argc, char const *argv[]) {
-	// initLogger wystarczy wywolac tylko raz na cale dzialanie programu
 	initLogger("p2p-client");
 	syslogger->info("p2p client starting");
 
 	SSocket testTrackerSocket("127.0.0.1", PORT);
 	testTrackerSocket.start();
 	testTrackerSocket.sendOk();
->>>>>>> master
 }
