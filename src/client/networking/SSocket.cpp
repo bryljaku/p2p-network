bc1d07f4e0b8ef094e511e8d0adbcfa381f5dc66
p.zmyslony <p.zmysl@gmail.com>
2020-04-20 17:20:55 +0200

    small fixes to SSocket

--- src/client/networking/SSocket.cpp
+++ src/client/networking/SSocket.cpp
@@ -106,21 +106,25 @@ TcpCode SSocket::sendOk() {
 	return CONNERROR;
 }
 
-Ips SSocket::sendSeedlistRequest() {
+Ips SSocket::sendSeedlistRequest(uint64_t hashedTorrent) {
 	TcpMessage t;
 	t.set_code(TcpCode::CS_SEEDLIST_REQUEST);
+	SeedlistRequest n;
+	n.set_hashedtorrent(hashedTorrent);
+	t.set_allocated_seedlistrequest(&n);
 	send(&t);
 
 	if(state == SENT) {
 		receive();
 		if(state == RECVD && lastMsg.code()==CS_SEEDLIST_RESPONSE) {
 			syslogger->debug(lastMsg.seedlistresponse().ipv4peers().at(0)); //TODO usunac bo test
+			// TODO: zrobic strukture Ips z tych otrzymanych adresow
 		}
 	}
 	return Ips();
 }
 
-size_t SSocket::sendNewTorrentRequest(Torrent torrent) {
+uint64_t SSocket::sendNewTorrentRequest(Torrent torrent) {
 	TcpMessage t;
 	t.set_code(TcpCode::CS_NEW_REQUEST);
 	NewRequest n;
@@ -134,6 +138,7 @@ size_t SSocket::sendNewTorrentRequest(Torrent torrent) {
 		if(state == RECVD && lastMsg.code()==CS_NEW_RESPONSE) {
 			size_t recvdHash = lastMsg.newresponse().newhash();
 			syslogger->debug("Received new hash: " + std::to_string(recvdHash));
+			return recvdHash;
 		}
 	}
 	return 0;
