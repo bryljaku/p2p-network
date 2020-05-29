0da4643a3c07d75ed1b702bfa85a98be1be80e57
p.zmyslony <p.zmysl@gmail.com>
2020-05-29 15:09:25 +0200

    thread safe now

--- src/server/database/Database.cpp
+++ src/server/database/Database.cpp
@@ -4,16 +4,19 @@
 
 
 void Database::addOrUpdateClient(ClientInfo clientInfo) {
+	mutex.lock();
     for (auto& c: clients)
         if (clientInfo.getAddress() == c.getAddress()) {
             c.setTorrents(clientInfo.getTorrents());
             return;
         }
     clients.emplace_back(clientInfo);
+	mutex.unlock();
 }
 void Database::deleteClient(const Id& clientId) {
     auto oldSize = clients.size();
     std::vector<ClientInfo>::iterator new_end;
+    mutex.lock();
     new_end = std::remove_if(clients.begin(), clients.end(),
                              [clientId](const ClientInfo &client) { return client.getId() == clientId; });
     clients.erase(new_end, clients.end());
@@ -22,17 +25,22 @@ void Database::deleteClient(const Id& clientId) {
         spdlog::info("Deleted client with id {}", clientId);
     else
         spdlog::info("Client with id {} not found. Not deleted", clientId);
+    mutex.unlock();
 }
 
 std::vector<ClientInfo> Database::getClients() {
-    return clients;
+	mutex.lock_shared();
+	std::vector<ClientInfo> ret = clients;
+	mutex.unlock_shared();
+    return ret;
 }
 
 bool Database::isHashUnique(size_t hash) {
+	mutex.lock_shared();
 	for(const auto& torrent : torrents)
 	    if (torrent.hashed == hash)
 			return false;
-		
+	mutex.unlock_shared();
 	return true;
 }
 
@@ -44,20 +52,34 @@ size_t Database::addTorrent(Torrent t) {
 		salt++;
 	}
 
+	mutex.lock();
 	torrents.push_back(t);
+	mutex.unlock();
 
 	return t.hashed;
 }
 
 std::vector<ClientInfo> Database::getClientsWith(Torrent torrent) {
     std::vector<ClientInfo> clientsToReturn;
+	mutex.lock_shared();
     for (auto i: clients)
         if (i.hasTorrent(torrent))
             clientsToReturn.emplace_back(i);
 
     if (clientsToReturn.empty())
         syslogger->warn("no peers for torrent {}", torrent.hashed);
+    mutex.unlock_shared();
     return clientsToReturn;
+}
 
+bool Database::hasTorrent(Torrent torrent) {
+	mutex.lock_shared();
+	for(auto t : torrents) {
+		if (t == torrent) {
+			return true;
+		}
+	}
+	mutex.unlock_shared();
+	return false;
 }
 
--- src/server/database/Database.h
+++ src/server/database/Database.h
@@ -6,10 +6,12 @@
 #include "ClientInfo.h"
 #include "Torrent.h"
 #include <sharedUtils.h>
+#include <shared_mutex>
 
 class Database {
     std::vector<ClientInfo> clients;
     std::vector<Torrent> torrents;
+    std::shared_mutex mutex;
 public:
     std::vector<ClientInfo> getClientsWith(Torrent torrent);
 
@@ -18,6 +20,7 @@ public:
     void deleteClient(const Id& clientId);
 
     bool isHashUnique(size_t hash);
+    bool hasTorrent(Torrent t);
     size_t addTorrent(Torrent t);		// returns new hash <- even if t has a set hash, it can be added with a different one (when there would be duplicate hashes)
 };
 
